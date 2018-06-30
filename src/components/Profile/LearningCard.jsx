bbf126963a1df2211ced258fcb65e21c19f1a249
Michelle Brien <michelle.brien12@gmail.com>
2018-06-30 15:53:00 +0100

    styled completed

--- src/components/Profile/LearningCard.jsx
+++ src/components/Profile/LearningCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Card, Dimmer, Icon, Image } from 'semantic-ui-react'
+import { Button, Card, Dimmer, Header, Icon, Image } from 'semantic-ui-react'
 import '../../css/Profile.css';
 
 // Refactor button out and use component once using props
@@ -44,7 +44,10 @@ export default class LearningCard extends Component {
 
           <Card>
           <Card.Content>
-            <Card.Header><span className="number">7</span> Completed</Card.Header>
+            <Header as='h3'>
+              <Icon name='check circle outline' color="green" />
+              <Header.Content><span className="green-number">5</span> Completed</Header.Content>
+            </Header>
           </Card.Content>
             <Image 
             label={{ as: 'a', color: 'green', content: 'Most recent', icon: 'circle check outline', ribbon: true }} src='https://javascript30.com/images/JS3-social-share.png' />
