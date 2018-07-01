a58597f76819fbc4bf19e993f4251266148588e6
Michelle Brien <michelle.brien12@gmail.com>
2018-07-01 14:23:18 +0100

    more test db content

--- src/test-db.js
+++ src/test-db.js
@@ -19,42 +19,60 @@ const data = {
         name: "Javascript",
         category: "Software development",
         learning: {
-          name: "Javascript 30",
-          description: "Wes Bos vanilla JS",
-          source: "Wes Bos",
-          url: "https://javascript30.com/",
-          imageUrl: "https://javascript30.com/images/JS3-social-share.png",
-          status: "in progress",
-          completedDate: "",
-          votesNum: 3,
+          1: {
+            name: "Javascript 30",
+            description: "Wes Bos vanilla JS",
+            source: "Wes Bos",
+            url: "https://javascript30.com/",
+            imageUrl: "https://javascript30.com/images/JS3-social-share.png",
+            status: "in progress",
+            completedDate: "",
+            votesNum: 3,
+          },
+          2: {
+            name: "Beginners guide to ES6",
+            description: "Basics of syntax",
+            source: "Udacity",
+            url: "https://eu.udacity.com/course/es6-javascript-improved--ud356",
+            imageUrl: "",
+            status: "completed",
+            completedDate: "20/06/18",
+            votesNum: 10,
+          }
         },
         making: {
-          name: "",
-          description: "",
-          url: "",
-          status: "",
-          votesNum: 0, 
+          1: {
+            name: "",
+            description: "",
+            url: "",
+            status: "",
+            votesNum: 0, 
+          }
         }
       },
       2: {
         name: "React",
         category: "Software development",
         learning: {
-          name: "React/Redux bootcamp",
-          description: "Intro to React",
-          source: "Le Wagon",
-          url: "https://www.lewagon.com/blog/introduction-to-react-js",
-          imageUrl: "",
-          status: "in progress",
-          completedDate: "",
-          votesNum: 0,
+          1: {
+            name: "React/Redux bootcamp",
+            description: "Intro to React",
+            source: "Le Wagon",
+            url: "https://www.lewagon.com/blog/introduction-to-react-js",
+            imageUrl: "",
+            status: "in progress",
+            completedDate: "",
+            votesNum: 0,
+          }
         },
         making: {
-          name: "Holiday poll",
-          description: "Dynamic leaderboard to help plan group holidays",
-          url: "https://github.com/mbrien12/holiday-poll",
-          status: "in progress",
-          votesNum: 0, 
+          1: {
+            name: "Holiday poll",
+            description: "Dynamic leaderboard to help plan group holidays",
+            url: "https://github.com/mbrien12/holiday-poll",
+            status: "in progress",
+            votesNum: 0, 
+          }
         }
       },
     }, 
