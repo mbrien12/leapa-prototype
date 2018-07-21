004435ef390c4adef75ab474152fc51bb62a669e
Michelle Brien <michelle.brien12@gmail.com>
2018-07-21 22:05:11 +0100

    restyled homepage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,12 @@ import '../css/LandingPage.css'
 export default class LandingPage extends Component {
   render() {
     return (
-     <div className="banner-cover">
+     <div>
+        <div className="banner-cover">
+        </div>
+        <div className="homepage-second-row">
+          <h2>The overall vision...</h2>
+        </div>
      </div>
     );
   }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ export default class Navigation extends Component {
   render() {
     return (
       <div className="top-navigation">
-       <a href='/'> <img className="logo-nav" src="https://image.ibb.co/mHF9EJ/icon_above_font.png" alt="logo"/></a>
+       <a href='/'> <p className="logo-nav">LeaPa</p></a>
       <ul>
         <li><a href="/profile">Profile</a></li>
         <li><a href="#">Log out</a></li>
