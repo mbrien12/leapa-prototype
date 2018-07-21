77a89c18026a117b1c1de5f7b7d0046b642d5c37
Michelle Brien <michelle.brien12@gmail.com>
2018-07-21 22:28:35 +0100

    started styling homepage;

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,6 +7,9 @@ export default class LandingPage extends Component {
     return (
      <div>
         <div className="banner-cover">
+          <div className="white">
+          <img className="image-test"src="https://image.ibb.co/fnHTPJ/Leapa_learning_track.png" alt=""/>
+          </div>
         </div>
         <div className="homepage-second-row">
           <h2>The overall vision...</h2>
